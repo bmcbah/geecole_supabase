2e436485eba67b2acaa14ce96c236fe13498be09
bmcbah <bahcelloum@gmail.com>
2026-07-17 17:55:27 +0200

    style: load Tailwind before legacy styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
+import "./shared/styles/tailwind.css";
 import "./shared/styles/global.css";
 import { App } from "./app/App";
 
